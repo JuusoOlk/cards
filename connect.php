dd5b60f06b4eeaa8173dba14d224d89ba9f3e98f
Juuso <a1704528@myy.haaga-helia.fi>
2019-10-06 15:22:41 +0300

    Modified connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,9 @@
 <?php
 function connect_db() {
  $servername = "localhost";
- // EDIT HERE YOUR USERNAME AND PASSWORD
- $username = "cards";
- $password = "password";
- $dbname = "cards";
+ $username = "a1704528";
+ $password = "viFAIU28i";
+ $dbname = "a1704528";
  // Create connection
  $conn = new mysqli($servername, $username, $password, $dbname);
  // Check connection
